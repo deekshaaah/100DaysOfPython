7cb4784e5cfeed0762593e80e2bba4911ca11182
deeksha <145819105+deekshaaah@users.noreply.github.com>
2024-01-24 00:43:44 +0530

    Add files via upload

---
+++ Codes/tuple.py
@@ -0,0 +1,7 @@
+t = (3, 5, 7, 3, "Deeksha")
+print(t)
+
+if 7 in t:
+    print("Yes")
+else:
+    print("No")
---
+++ Codes/tuple_methods.py
@@ -0,0 +1,18 @@
+# tuple methods
+
+print("Merging two tuples together : ")
+tup = (3, 7, 2, 0)
+print("tup = ", tup)
+lol= (4,7,3,9)
+print("lol = ", lol)
+arv = tup + lol
+print(arv)
+
+print("Count the number of occurence : ")
+print(arv.count(3))
+
+print("First index of the desired element : ")
+print(arv.index(0))
+
+print("Length of tuple : ")
+print(len(arv))
