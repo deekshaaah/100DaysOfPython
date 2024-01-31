2b7ca190ad49892eb5f9c4a60cfa8db3f2b152cf
deeksha <145819105+deekshaaah@users.noreply.github.com>
2024-01-31 17:19:31 +0530

    Add files via upload

---
+++ Codes/dict.py
@@ -0,0 +1,18 @@
+dic = {
+    "Deeksha": "Profession",
+    "Shasha": "Social",
+    "Gungun": "Family Level",
+    }
+
+print(dic["Shasha"])
+
+# to access all keys
+
+print(dic.keys())
+
+# iteration
+
+for keys in  dic.keys():
+    print(dic[keys])
+
+print(dic.items())
