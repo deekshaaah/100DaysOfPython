f5ad16883ab3a1ff5337c51c5603a2667a4d2544
Deeksha Singh <145819105+deekshaaah@users.noreply.github.com>
2024-02-17 16:58:00 +0530

    Delete enumerate_function.py

--- enumerate_function.py
+++
@@ -1,7 +0,0 @@
-arr = [23, 45, 76, 23, 76, 45, 87, 34, 67, 98, 69]
-
-for index, i in enumerate (arr, start = 1):
-    print(i)
-    if (index == 6):
-        print("Now, I know how to use enumerate function!")
-    
